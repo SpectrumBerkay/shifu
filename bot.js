9f375ac8ac0c8132e1005106276f9f29d48cd2e8
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 20:15:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,9 @@ var prefix = ("fbebsi");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  
+});
 
+  
 client.on('message', msg => {
  if (msg.content.toLowerCase() === prefix + 'ping') {
     msg.reply('Pong! **' + client.ping + '** ms');
@@ -44,7 +45,7 @@ client.on('message', msg => {
       .setThumbnail(msg.author.avatarURL)
  msg.channel.sendEmbed(embed);
  break;
-
-      
-      
+  }
+});
+           
 client.login(process.env.BOT_TOKEN);
