dc5eea9b2f03232472f79648892769c4c5400368
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-31 19:22:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
       .setAuthor(msg.author.username)
       .setDescription("This is User Info!")
       .setColor(0xFFFFFF)
-      .setField("Created At", message.author.createdAt);
+      .setField("Created At", msg.author.createdAt);
       
  msg.channel.sendEmbed(embed);
  break;
