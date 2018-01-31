2e80378ff63c9df53b4bbb455d47ac97c3d45961
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-31 19:57:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
       .setAuthor("Kullanıcı Bilgileri!")
       .setDescription("")
       .setColor(0x00FFFF)
-      .setImage(msg.author.avatarURL)
+      .setThumbnail(msg.author.avatarURL)
       .addField("Tam Ad", msg.author.username)
       .addField("ID", msg.author.id)
       .addField("Hesabı Ne zaman Oluşturduğu", msg.author.createdAt);
